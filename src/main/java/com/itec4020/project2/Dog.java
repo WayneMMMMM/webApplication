ed60fbd0250700437484a09acad4eaf1fe5f8fdb
WayneMMMMM <43655399+WayneMMMMM@users.noreply.github.com>
2018-11-12 14:32:30 -0500

    Add files via upload

---
+++ src/main/java/com/itec4020/project2/Dog.java
@@ -0,0 +1,50 @@
+package com.itec4020.project2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+@WebServlet("/Dog")
+public class Dog extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public Dog() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int cursor;
+		FileInputStream in = new FileInputStream(new File("/home/grp3/dog.png"));
+		
+		response.setContentType("image/jpg");
+		
+		while((cursor = in.read())!=-1){
+            response.getOutputStream().write(cursor);    
+        }
+		in.close();
+		
+		
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
